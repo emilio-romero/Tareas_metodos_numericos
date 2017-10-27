cd662fe44a836c3e9e74e774abf3a1f361e41b6a
loconali lap <emilio.romerodlsantos@gmail.com>
2017-10-27 13:23:28 -0500

    Tarea 9: implementados polinomios de interpolacion, lagrange y newton

--- tarea09/src/interpolacion.c
+++ tarea09/src/interpolacion.c
@@ -13,7 +13,103 @@ double *polinomial(double *x0, double *y0, int n){
 double *c=(double*)malloc(n*sizeof(double));
 double **A=(double**)malloc(n*sizeof(double*));
 for(int i=0;i<n;i++) A[i]=(double*)malloc(n*sizeof(double));
-
-c=factLU(A,c,n);
+  c=factLU(A,c,n);
+for(int i=0;i<n;i++) free(A[i]);
+free(A);
 
 return(c);}
+
+double evalPolinomio(double *c, double x, int n){
+double px=0.0; 
+  for(int i=0;i<n;i++)
+    px+=c[i]*pow(x,i);
+return(px);}
+
+void interpolaPolinomio(double *x0, double *y0,double xi, double xf,int n){
+double *c;//=(double*)malloc(n*sizeof(double));
+  c=polinomial(x0,y0,n);
+double dx=xf-xi/(double)(4*n);
+double **ndata=(double**)malloc(4*n*sizeof(double*));
+  for(int i=0;i<4*n;i++){
+    ndata[i]=(double*)malloc(2*sizeof(double));
+    ndata[i][0]=xi+i*dx; 
+    ndata[i][1]=evalPolinomio(c,ndata[i][0],n);
+  }
+  writeMatrix(ndata,4*n,2,"polinomial_data.dat");
+for(int i=0;i<4*n;i++) free(ndata[i]);
+free(ndata);
+free(c);
+}
+
+/*Polinomios de Lagrange*/
+double Lagrange(double *x0, double x, int n, int i){
+  double Lx=1; 
+  for(int j=0;j<n;j++){
+    if(j!=i)
+      Lx*=(x-x0[j])/(x0[i]-x0[j]);
+  }
+return Lx;}
+
+double evalLagrangep(double *x0, double *y0, double x, int n){
+double px=0.0;
+  for(int i=0;i<n;i++){
+    px+=y0[i]*Lagrange(x0,x,n,i);
+  }
+return(px);}
+
+void interpolaLagrange(double *x0, double *y0,double xi, double xf,int n){
+double dx=xf-xi/(double)(4*n);
+double **ndata=(double**)malloc(4*n*sizeof(double*));
+  for(int i=0;i<4*n;i++){
+    ndata[i]=(double*)malloc(2*sizeof(double));
+    ndata[i][0]=xi+i*dx; 
+    ndata[i][1]=evalLagrangep(x0,y0,ndata[i][0],n);
+  }
+  writeMatrix(ndata,4*n,2,"poliLagrange_data.dat");
+for(int i=0;i<4*n;i++) free(ndata[i]);
+free(ndata);
+}
+/*Polinomios de Newton*/
+double Newtonp(double *x0, double x, int n, int i){
+ if(i!=0){
+ double Nx=1; 
+  for(int j=0;j<i;j++){
+    Nx*=(x-x0[j]);
+  }
+ return Nx;
+ }
+ else return 1.0;
+}
+
+void diferenciasDivididas(double **A, double *x0, double *y0, int n){
+
+  for(int i=0;i<n;i++) A[i][0]=y0[i]; 
+  for(int j=1;j<n;j++){
+    for(int i=0;i<n-j;i++){
+      A[i][j]=A[i+1][j-1]-A[i][j-1]/(x0[i+j]-x0[i]);
+    }
+  }
+}
+
+double evalNewtonp(double **A, double *x0, double x, int n){
+double px=0; 
+  for(int i=0;i<n;i++){
+    px+=A[0][i]*Newtonp(x0,x,n,i);
+  }
+return(px);}
+
+void interpolaNewton(double *x0, double *y0,double xi, double xf,int n){
+double dx=xf-xi/(double)(4*n);
+double **ndata=(double**)malloc(4*n*sizeof(double*));
+double **A=(double**)malloc(n*sizeof(double*));
+for(int k=0;k<n;k++) A[k]=(double*)malloc(n*sizeof(double));
+diferenciasDivididas(A, x0, y0, n);
+  for(int i=0;i<4*n;i++){
+    ndata[i]=(double*)malloc(2*sizeof(double));
+    ndata[i][0]=xi+i*dx; 
+    ndata[i][1]=evalNewtonp(A,x0,ndata[i][0],n);
+  }
+  writeMatrix(ndata,4*n,2,"poliNewton_data.dat");
+for(int i=0;i<4*n;i++) free(ndata[i]);
+free(ndata);
+}
--- tarea09/src/interpolacion.h
+++ tarea09/src/interpolacion.h
@@ -4,7 +4,16 @@
 #include <stdlib.h> 
 #include <math.h> 
 #include "solucionadores.h"
+#include "lectura.h"
 void Vandermonde(double **A, double *x, int n);
 double *polinomial(double *x0, double *y0, int n);
-
+double evalPolinomio(double *c, double x, int n);
+void interpolaPolinomio(double *x0, double *y0, double xi, double xf, int n);
+double Lagrange(double *x0, double x, int n, int i);
+double evalLagrangep(double *x0, double *y0, double x, int n);
+void interpolaLagrange(double *x0, double *y0, double xi, double xf, int n);
+double Newtonp(double *x0, double x, int n, int i);
+void diferenciasDivididas(double **A, double *x0, double *y0, int n);
+double evalNewtonp(double **A, double *x0, double x, int n);
+void interpolaNewton(double *x0, double *y0, double xi, double xf, int n);
 #endif 
